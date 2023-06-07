bc9ee8bf1d985190270ce5592e9244c615b6d89f
Henrique-mazzoleni <henrique.mazzoleni@gmail.com>
2023-06-07 19:18:49 +0200

    added HTMLReport

--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,14 @@ import { WinAnalyser } from './analysers/WinAnalyser';
 import { AverageGoalsAnalysis } from './analysers/AverageGoalsAnalysis';
 import { ConsoleReport } from './reportTargets/ConsoleReport';
 import { Summary } from './Summary';
+import { HTMLReport } from './reportTargets/HTMLReport';
 
 const reader = new MatchReader(new CsvFileReader('football.csv'));
 reader.load();
 const { matches } = reader;
-const report = new ConsoleReport();
+const report = new HTMLReport();
 
-const manWinsSummary = new Summary(
-  new WinAnalyser('Man United'),
-  new ConsoleReport()
-);
+const manWinsSummary = new Summary(new WinAnalyser('Man United'), report);
 manWinsSummary.buildAndPrintReport(matches);
 
 const manAvgGoalsSummary = new Summary(
